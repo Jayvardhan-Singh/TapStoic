0f6fa7652eb20baaa8dc99131509ac4ec90b23bf
Jayvardhan Singh <jayvardhanmd@gmail.com>
2026-02-16 13:20:19 +0530

    Add Sidebar

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.header("TapStoic")
+
+st.sidebar.markdown("**About the App:** Training to Increase *Attention Accuracy* while *Emotion turmoil* Can provide a time-window in *Real-Life* High-stakes Situations")
