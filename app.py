f3488dd22b4eabfbcd4927c426b2c22b6f3f601b
Jayvardhan Singh <jayvardhanmd@gmail.com>
2026-02-20 10:03:20 +0530

    Add 5-Point Summary & Record Audio

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 st.title("TapStoic")
 
 st.sidebar.markdown("**About the App:** Train to Increase *Attention Accuracy* during *Emotion turmoil* Can provide a time-window in *Real-Life* High-stakes Situations")
+st.sidebar.markdown("Step-wise instructions: 1. Record your thought that is looping inside your mind; 2. Play it and point the cursor on Tap button; 3. Close your eyes and Tap at the start of each Inspiration till the record is playing")
 
 st.header("Tap Interval Timer")
 st.subheader("Tap on start of each Inspiration")
@@ -14,6 +15,14 @@ if 'last_tap_time' not in st.session_state:
     st.session_state.last_tap_time = None
 if 'intervals' not in st.session_state:
     st.session_state.intervals = []
+
+
+# Record audio from the user's microphone
+audio_file = st.audio_input("Record a voice message")
+
+# If an audio file is recorded, display the playback
+if audio_file:
+    st.audio(audio_file)
 
 # Tap Button
 if st.button("Tap"):
@@ -32,11 +41,13 @@ if st.button("Tap"):
 
 # Display data
 if st.session_state.intervals:
-    df = pd.DataFrame(st.session_state.intervals, columns=["Intervals (seconds)"])
-    st.dataframe(df)
+    df = pd.DataFrame(st.session_state.intervals, columns=["Intervals_Seconds"])
+   
 # User can Stop the training by pressin STOP button.
 if st.button("STOP"):
     st.session_state.last_tap_time = None
     st.session_state.intervals = []
     st.bar_chart(df.iloc[1:-1])  # To exclude first Two and last interval from Chart
     st.line_chart(df.iloc[1:-1])  # To exclude first Two and last interval from Chart
+    st.dataframe(df) # Of all the data without exclusion 
+    st.table(df.iloc[1:-1].describe()) #To exclude first Two and last interval from Analysis
